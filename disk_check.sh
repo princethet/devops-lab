003045dff551444f0b97b6fb088144000561cbb8
Prince <princetheta003@gmail.com>
2026-02-03 13:49:54 +0000

    ADD disk monitoring with Slack alerts using enviroment variables

--- disk_check.sh
+++ disk_check.sh
@@ -6,10 +6,15 @@ USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
 echo "Disk usage: ${USAGE}%"
 
 if [ "$USAGE" -ge "$THRESHOLD" ]; then
+  MESSAGE="🚨 *Disk Alert* 🚨\nHost: $(hostname)\nDisk Usage: ${USAGE}%\nThreshold: ${THRESHOLD}%\nTime: $(date)"
+
+  curl -s -X POST -H 'Content-type: application/json' \
+    --data "{\"text\":\"$MESSAGE\"}" \
+    "$SLACK_WEBHOOK_URL"
+
   echo "ALERT: Disk usage HIGH (>= ${THRESHOLD}%)"
-  exit 1   # 👈 CI ko FAIL karne ke liye
+  exit 1
 else
   echo "OK: Disk usage normal (< ${THRESHOLD}%)"
   exit 0
 fi
-
